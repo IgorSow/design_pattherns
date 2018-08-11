18bf7ce8cf51d65364e2b1a1480ced566d332e1c
Igor <sowinski.igor@gmail.com>
2018-08-11 11:33:43 +0200

    adder pattern Builder

--- src/main/java/com/sda/Application.java
+++ src/main/java/com/sda/Application.java
@@ -1,4 +1,49 @@
 package com.sda;
 
+import com.sda.model.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import java.time.Instant;
+import java.util.Arrays;
+
 public class Application {
+
+    public static void main(String[] args) {
+
+        Document documentByConstructor = new Document(
+                "Plan Dnia",
+                "Opis dnia szkoleniowego",
+                Instant.now(),
+                "Igor",
+                Arrays.asList("długopis", "zeszyt", "laptop")
+        );
+
+        // setters
+
+        Document documentBySetters = new Document();
+
+        documentBySetters.setTitle("Plan tygodnia");
+        documentBySetters.setDescription("Tutaj jest plan tygodnia");
+        documentBySetters.setAuthor("Igor Sowinski");
+        documentBySetters.setCreationDate(Instant.now());
+        documentBySetters.setItems(Arrays.asList("długopis", "zeszyt", "laptop"));
+
+        System.out.println("Hello Word");
+
+
+        Document document2 = new Document();
+
+        //builder
+
+        Document documentByBuilder = Document.builder()
+                .title("Nowy plan")
+                .author("Jarek")
+                .items(Arrays.asList("długopis", "zeszyt", "laptop"))
+                .creationDate(Instant.now())
+                .description("Tutaj opis")
+                .item("DODANE POZNIEJ")
+                .build();
+
+
+    }
 }
