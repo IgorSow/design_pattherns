2f207e796f46d2d7743e040cea7d3abc0b928519
Igor <sowinski.igor@gmail.com>
2018-08-12 08:59:43 +0200

    singleton 1#

---
+++ src/main/java/com/sda/singleton/Consumer.java
@@ -0,0 +1,17 @@
+package com.sda.singleton;
+
+public class Consumer implements Runnable {
+
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("consumer " + Queue.instanceOf().pop());
+        }
+    }
+}
