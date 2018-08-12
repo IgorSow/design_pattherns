762dbbbe8325e5fba8a9c63e8172002013c23c80
Igor <sowinski.igor@gmail.com>
2018-08-12 11:42:57 +0200

    strategy 1#

---
+++ src/main/java/com/sda/strategy/PaymentStrategy.java
@@ -0,0 +1,12 @@
+package com.sda.strategy;
+
+import com.sda.adapter.domain.Payment;
+
+import java.util.List;
+
+public class PaymentStrategy {
+
+    public Payment selectBestPayment(PaymentModel paymentModel, List<Payment> paymentList){
+        return null;
+    }
+}
